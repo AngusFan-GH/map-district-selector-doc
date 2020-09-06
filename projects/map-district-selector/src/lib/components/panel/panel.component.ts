2d75486640fe54aa316f1b77b2893808f9716812
Angus <angus@Lucifer.com>
2020-09-07 00:01:54 +0800

    feat: temp update

--- projects/map-district-selector/src/lib/components/panel/panel.component.ts
+++ projects/map-district-selector/src/lib/components/panel/panel.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MapDistrictSelectorService } from '../../map-district-selector.service';
 import { ReadJsonService } from '../../utils/read-json/read-json.service';
-import { EChartOption } from 'echarts';
+import { ECharts } from 'echarts';
+import * as echarts from 'echarts';
+import { zip, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'mds-panel',
@@ -9,27 +11,65 @@ import { EChartOption } from 'echarts';
   styleUrls: ['./panel.component.scss']
 })
 export class PanelComponent implements OnInit {
-  mapOptions: EChartOption = null;
+  @ViewChild('mapChart') mapChart: ElementRef;
+  chart: ECharts;
+  options: any = {
+    title: {
+      text: '全国34个省市自治区',
+      subtext: '中国'
+    },
+    series: [
+      {
+        type: 'map',
+        mapType: 'China',
+        selectedMode: 'single',
+        itemStyle: {
+          normal: {
+            areaColor: '#AAD5FF',
+            borderColor: 'white',
+            label: { show: true, color: 'white' }
+          },
+          emphasis: {
+            areaColor: '#A5DABB'
+          }
+        },
+      }
+    ]
+  };
   constructor(
     private helper: MapDistrictSelectorService,
-    private readJson: ReadJsonService
-  ) {
-    this.readJson.readJson('data').subscribe(data => {
-      // this.mapOptions = {
-      //   series: [
-      //     {
-      //       name: '随机数据',
-      //       type: 'map',
-      //       mapType: 'china',
-      //       selectedMode: 'single',
-      //       data
-      //     }
-      //   ]
-      // };
+    private readJson: ReadJsonService,
+  ) { }
+
+  ngOnInit(): void {
+    this.readJson.getJsonFromApi('https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json')
+      .subscribe(ChinaJson => {
+        console.log(ChinaJson);
+
+        echarts.registerMap('China', ChinaJson);
+        this.options.series[0].data = ChinaJson;
+        this.chart = echarts.init(this.mapChart.nativeElement);
+        this.chart.setOption(this.options);
+        this.bindMapSelectChangedEvent();
+      });
+  }
+
+  bindMapSelectChangedEvent(): void {
+    fromEvent(this.chart, 'mapselectchanged').subscribe((param: any) => {
+      const selectedName = param.batch[0].name;
+      this.readJson.readJson(selectedName).subscribe(e => {
+        echarts.registerMap(selectedName, e);
+        this.options.series[0].mapType = selectedName;
+        this.options.title.subtext = selectedName === 'China' ? '中国' : selectedName;
+        this.chart.setOption(this.options, true);
+      });
     });
   }
 
-  ngOnInit(): void {
+  back(): void {
+    this.options.series[0].mapType = 'China';
+    this.options.title.subtext = '中国';
+    this.chart.setOption(this.options, true);
   }
 
   close(): void {
--- projects/map-district-selector/src/lib/components/panel/panel.module.ts
+++ projects/map-district-selector/src/lib/components/panel/panel.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PanelComponent } from './panel.component';
-import { NgxEchartsModule } from 'ngx-echarts';
+import { NgxEchartsModule, NGX_ECHARTS_CONFIG } from 'ngx-echarts';
 import { ReadJsonModule } from '../../utils/read-json/read-json.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgxEchartsModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    }),
     ReadJsonModule
   ],
   declarations: [PanelComponent],
   exports: [PanelComponent],
-  entryComponents: [PanelComponent]
+  entryComponents: [PanelComponent],
+  providers: [
+    { provide: NGX_ECHARTS_CONFIG, useValue: echarts }
+  ]
 })
 export class MDSPanelModule { }
