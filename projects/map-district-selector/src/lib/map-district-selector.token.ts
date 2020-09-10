8a095aebecec857b97442d26a51de516f2a071c4
Angus <angus@Lucifer.com>
2020-09-11 00:38:13 +0800

    deploy demo

--- projects/map-district-selector/src/lib/map-district-selector.token.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const MAP_DISTRICT_SELECTOR_CLOSE_FUNC_TOKEN = new InjectionToken('MAP_DISTRICT_SELECTOR_CLOSE_FUNC_TOKEN');
-
-export const GEO_JSON_API_PATH = new InjectionToken<{}>('GEO_JSON_API_PATH');
--- projects/map-district-selector/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of map-district-selector
- */
-
-export * from './lib/map-district-selector.service';
-export * from './lib/map-district-selector.module';
-export * from './lib/map-district-selector.token';
-export * from './lib/map-district-selector.types';
